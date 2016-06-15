f595ac8a329f7099e04e2570176a9e8ceb8f26e1
Clemens Kirchgatterer <clemens@1541.org>
2016-06-15 18:16:42 +0200

    cleanup

--- http.cpp
+++ http.cpp
@@ -50,21 +50,20 @@ static String format_bytes(size_t bytes) {
 static String get_content_type(AsyncWebServerRequest *request,
                                const String &filename) {
 
-       //(request->hasArg("download")) return ("application/octet-stream");
-       if (filename.endsWith(".htm"))   return ("text/html");
-  else if (filename.endsWith(".html"))  return ("text/html");
-  else if (filename.endsWith(".css"))   return ("text/css");
-  else if (filename.endsWith(".js"))    return ("application/javascript");
-  else if (filename.endsWith(".png"))   return ("image/png");
-  else if (filename.endsWith(".gif"))   return ("image/gif");
-  else if (filename.endsWith(".jpg"))   return ("image/jpeg");
-  else if (filename.endsWith(".ico"))   return ("image/x-icon");
-  else if (filename.endsWith(".xml"))   return ("text/xml");
-  else if (filename.endsWith(".mp3"))   return ("audio/mpeg");
-  else if (filename.endsWith(".pdf"))   return ("application/x-pdf");
-  else if (filename.endsWith(".zip"))   return ("application/x-zip");
-  else if (filename.endsWith(".gz"))    return ("application/x-gzip");
-                                        return ("text/plain");
+       if (filename.endsWith(".html")) return ("text/html");
+  else if (filename.endsWith(".htm"))  return ("text/html");
+  else if (filename.endsWith(".css"))  return ("text/css");
+  else if (filename.endsWith(".xml"))  return ("text/xml");
+  else if (filename.endsWith(".png"))  return ("image/png");
+  else if (filename.endsWith(".gif"))  return ("image/gif");
+  else if (filename.endsWith(".jpg"))  return ("image/jpeg");
+  else if (filename.endsWith(".ico"))  return ("image/x-icon");
+  else if (filename.endsWith(".mp3"))  return ("audio/mpeg");
+  else if (filename.endsWith(".pdf"))  return ("application/pdf");
+  else if (filename.endsWith(".zip"))  return ("application/zip");
+  else if (filename.endsWith(".gz"))   return ("application/gzip");
+  else if (filename.endsWith(".js"))   return ("application/javascript");
+                                       return ("text/plain");
 }
 
 static void create_session_key(void) {
